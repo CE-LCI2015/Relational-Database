5d71c97d46cf2287ffcff5bcb5e43d4cc12cbf5d
pablo <paroque28@gmail.com>
2015-08-10 16:22:04 -0600

    update transaction

--- Program/database.rkt
+++ Program/database.rkt
@@ -8,38 +8,30 @@
         (letrec
 
             ; These strings are used as prompts
-
            ((WELCOME "Welcome to myDatabase \n >> ")
-            (PROMPT ">> ")
-            (AT "Enter transaction (- for withdrawal): ")
-            (FB "Your final balance is: ")
+            (PROMPT "\n>> ")
+            (ERROR_INPUT "Unknown command")
 
             ; This function displays a prompt then returns
             ; a value read.
-
             (prompt-read (lambda (Prompt)
 
                   (display Prompt)
                   (read)))
 
-            ; This function recursively computes the new
-            ; balance given an initial balance init and
-            ; a new value t.  Termination occurs when the
-            ; new value is 0.
 
-            (newbal (lambda (Init t)
-                  (if (= t 0)
-                      (list FB Init)
-                      (transaction (+ Init t)))))
+            
 
-            ; This function prompts for and reads the next
-            ; transaction and passes the information to newbal
-
-            (transaction (lambda (Init)
-                      (newbal Init (prompt-read AT)))))
-
-; This is the body of checkbook;  it prompts for the
-; starting balance
+            (transaction (lambda (command)
+                           (cond
+                             [(or (equal? command "addtable") (equal? command "addt"))] (display "addtable")
+                             [#t](display ERROR_INPUT)
+                             )
+                          
+                           (transaction (prompt-read PROMPT)))))
 
   (transaction (prompt-read WELCOME)))))
+
+
+
 (database)
