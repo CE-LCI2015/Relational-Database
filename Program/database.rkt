f8550fef6c913ca61c46c792c8a21c893147a3f4
pablo <paroque28@gmail.com>
2015-08-15 16:18:52 -0600

    Finish of insert

--- Program/database.rkt
+++ Program/database.rkt
@@ -2,11 +2,11 @@
 #lang scheme
 (require "utilities.rkt")
 
-(define (WELCOME) "Welcome to myDatabase \n >> ")
+(define (WELCOME) "Welcome to RELDB \n >> ")
 (define (PROMPT) "\n>> ")
 (define (ERROR_INPUT)  "Unknown command: ")
-(define (ERROR_ARGUMENTS) "Unsuficient number of arguments or wrong command")
-
+(define (ERROR_ARGUMENTS) "Unsuficient number of arguments or wrong command.\n")
+(define (WRONG_TABLE) "Wrong table selected.\n")
 
 
 (define prompt-read (lambda (Prompt)
@@ -31,19 +31,44 @@
 (define manageCommandAux (lambda (db list)
                                 (cond
                              [(or (equal? (car list) "addtable") (equal? (car list) "addt")) (addtable db (cdr list))]
-                             [#t (display (string-append (ERROR_INPUT) (car list)))]
+                             [(or (equal? (car list) "insert") (equal? (car list) "ins")) (insert db (cdr list))]
+                             [#t (display (string-append (ERROR_INPUT) (car list) "\n")) db]
                              ))
 )
 
-(define addtable (lambda(db args)
-                        (let ([num (length args)])
-                          (cond
-                            [(= num 1)(display (ERROR_ARGUMENTS))]
-                            [#t (cons args db)])
-                          );end let
+; Returns index of table in db
+(define searchtable (lambda(db tablename [index 0])
+                        (cond
+                          [(null? db) -1]
+                          [(equal? tablename (caaar db)) index]
+                          [#t (searchtable (cdr db) tablename (+ 1 index))]
+                          )
+                        )
+)
+; Inserts record on a table PD: does not checks length of db vs index of table
+(define insertrecord (lambda(db index record)
+                       (cond
+                         [(> index 0) (cons (car db) (insertrecord (cdr db) (- 1 index) record))]
+                         [#t (cons (append (car db) (list record)) (cdr db))]
+                         )
                         )
 )
 
+
+(define addtable (lambda(db args)
+                          (cond
+                            [(= (length args) 1)(display (ERROR_ARGUMENTS)) db]
+                            [#t (cons (list args) db)])                        
+                        )
+)
+
+(define insert (lambda(db args)
+                (cond
+                  [(equal? (searchtable db (car args)) -1) (display (WRONG_TABLE)) db]
+                  [(equal? (length (car (getPos db (searchtable db (car args))))) (length args) ) (insertrecord db (searchtable db (car args)) (cdr args))]
+                  )      
+                )
+)
 
   
 (define (database) (manageCommand '() (prompt-read (WELCOME))));end database
